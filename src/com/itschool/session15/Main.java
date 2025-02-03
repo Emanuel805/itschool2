711843b2672cceea1f41d92add53edf831134f3d
Emanuel805 <trandafiremanuel440@gmail.com>
2025-02-03 16:45:33 +0200

    new fix

---
+++ src/com/itschool/session15/Main.java
@@ -0,0 +1,11 @@
+package com.itschool.session15;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat myCat = new Cat();
+        Dog myDog = new Dog();
+
+        myCat.cats(); // Output: Cats meow
+        myDog.dogs(); // Output: Dogs bark
+    }
+}
